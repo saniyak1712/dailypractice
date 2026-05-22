f74750d3185abd4ecd3cb65ca76b30b9e394fd58
saniyak1712 <readytofire17@gmail.com>
2026-05-22 15:26:09 +0530

    Defanging an IP Address

---
+++ Defanging an IP Address.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        int index = 0;
+        string ans ;
+
+        while(index < address.size()){
+            if(address[index] == '.'){
+                ans +="[.]";
+                index++;
+            }
+            else{
+                ans += address[index];
+                index++;
+            }
+        }
+        return ans;
+    }
+};
