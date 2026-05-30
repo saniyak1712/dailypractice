9f498ddda7b4193df52abd80649fff2cc84ec42b
Saniya Khandelwal <readytofire17@gmail.com>
2026-05-30 23:34:54 +0530

    Implement function to count special character pairs

---
+++ Count the Number of Special Characters I.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        vector<int> lower(26,0);
+        vector<int> upper(26, 0);
+       
+
+        for(char ch: word){
+
+            if(islower(ch))
+
+            lower[ch - 'a'] =  1;
+
+            else{
+                upper[ch - 'A'] =  1;
+            }
+        }
+
+        int count = 0;
+        for(int i =0; i<26; i++){
+            if(lower[i] && upper[i])
+            count++;
+        }
+        return count;
+    }
+};
