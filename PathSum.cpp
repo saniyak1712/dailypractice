9ed4a8f54d1fae862180af40d5acd50ae3f0a71f
Saniya Khandelwal <readytofire17@gmail.com>
2026-06-10 11:40:56 +0530

    Implement hasPathSum function to check path sum

---
+++ PathSum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(root == nullptr) 
+        return false;
+        if(root -> left == nullptr && root->right == nullptr)
+        return root->val == targetSum;
+
+
+    
+    return hasPathSum(root->left, targetSum-root-> val) || hasPathSum(root->right, targetSum- root-> val);
+}
+
+};
